cdea107028a0ec3a5a6408b4aa2a0561affdc00b
USER <user@AL01457283.local>
2019-11-21 12:03:08 +0900

    fix memory usage

--- gen_DanceReID.py
+++ gen_DanceReID.py
@@ -3,140 +3,14 @@ import argparse
 from tqdm import tqdm
 from collections import defaultdict 
 import re
-import cv2
-import glob
 import json
+import logging
 
 import numpy as np
-from time import time
-from PIL import Image, ImageColor
+from util import parse_dict_track, draw_pose_cv2, gen_video_detections
 from dataset.video_dataset import VideoDataset
 
-
-DICT_POSE_COCO = dict()
-DICT_POSE_COCO['line_pair'] = [
-    (0, 1), (0, 2), (1, 3), (2, 4),  # Head
-    (5, 6), (5, 7), (7, 9), (6, 8), (8, 10),
-    (5, 11), (6, 12),  # Body
-    (11, 13), (12, 14), (13, 15), (14, 16)
-]
-DICT_POSE_COCO['point_col'] = [
-    'green', 'blue', 'blue', 'blue', 'blue', 'yellow', 'orange', 'yellow', 'orange', 'yellow', 'orange',
-    'purple', 'red', 'purple', 'red', 'purple', 'red'
-]
-DICT_POSE_COCO['acc_idx'] = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17)
-DICT_POSE_COCO['flip_ref'] = ((2, 3), (4, 5), (6, 7), (8, 9), (10, 11), (12, 13), (14, 15), (16, 17))
-
-def cropBox(img, pose, ul, br, resH, resW):
-
-    im_H = img.shape[0]
-    im_W = img.shape[1]
-
-    lenH = max(br[0] - ul[0], (br[1] - ul[1]) * resH / resW)
-    lenW = lenH * resW / resH
-
-    center = [ (br[0] + ul[0]) / 2, (br[1] + ul[1]) / 2]
-
-    new_ul = [max (0, int(center[0] - lenH / 2)), max (0, int(center[1] - lenW / 2))]
-    new_br = [min (im_H-1, int(center[0] + lenH / 2)), min (im_W-1, int(center[1] + lenW / 2))]
-
-    if new_ul[0] == 0:
-        new_br[0] = lenH
-    elif new_br[0] == im_H-1:
-        new_ul[0] = im_H-lenH
-
-    if new_ul[1] == 0:
-        new_br[1] = lenW
-    elif new_br[1] == im_W-1:
-        new_ul[1] = im_W-lenW
-
-
-    center = [(new_br[0] + new_ul[0]) / 2, (new_br[1] + new_ul[1]) / 2]
-
-    newImg = img[int(new_ul[0]) : int(new_br[0]), int(new_ul[1]) : int(new_br[1])].copy()
-
-    factor = float(resH) / lenH
-    newImg = cv2.resize(newImg, ( int(resW), int(resH) ) )
-
-    shift_pose = pose - np.array([center[1], center[0]])
-    shift_pose = shift_pose * factor + np.array([resW/2, resH/2])
-
-    return newImg, shift_pose
-
-def parse_dict_track(video_dataset, dict_track, valids, output_height = 256, output_width = 128):
-
-    frame_dict = defaultdict(dict)
-    id_dict = defaultdict(lambda: defaultdict(list))
-
-    for frame_idx, dict_poses in tqdm(dict_track.items()):
-
-        image = video_dataset.read_frame(frame_idx)
-
-        list_labels = []
-        list_images = []
-        list_image_pose = []
-
-        for bid, dict_pose in dict_poses.items():
-
-            pid = dict_pose['id'] - 1
-
-            if valids is not None:
-                if frame_idx not in valids[str(pid)]: continue
-
-            box_pos = dict_pose['box_pos']
-            up_left = [box_pos[2], box_pos[0]]
-            bot_right = [box_pos[3], box_pos[1]]
-
-            image_crop, shift_pose = cropBox(image, dict_pose['pose_pos'], up_left, bot_right, output_height, output_width)
-            image_pil = Image.fromarray(image_crop)
-
-            id_dict [pid]['frame_idx'].extend([frame_idx])
-            id_dict [pid]['images'].extend([image_pil])
-            id_dict [pid]['poses'].extend([shift_pose])
-
-    print('Parsing to dictionary done!!!')
-
-    return id_dict
-
-
-def draw_pose_cv2(img, pose_pos, pid, point_size=5, line_size=2, draw_pose_line=True):
-
-    img = np.asarray(img)
-
-    def convert_int_to_color(i):
-        list_colors = list(ImageColor.colormap)
-        return list_colors[i * 10 % len(list_colors)]
-
-
-    def convert_color_to_rgb(col, default_col='red'):
-        if col in ImageColor.colormap:
-            return ImageColor.getrgb(col)
-        else:
-            return ImageColor.getrgb(default_col)
-
-    line_col = convert_int_to_color(pid)
-
-    dict_pose_line = dict()
-    line_pair = DICT_POSE_COCO['line_pair']
-    point_col = [ImageColor.getrgb(col.strip()) for col in DICT_POSE_COCO['point_col']]
-    line_col = convert_color_to_rgb(line_col)
-
-    for i in range(pose_pos.shape[0]):
-        x, y = int(pose_pos[i, 0]), int(pose_pos[i, 1])
-        if x > 0 and y > 0:
-            dict_pose_line[i] = (x, y)
-            cv2.circle(img, (x, y), point_size, point_col[i], -1)
-
-    if draw_pose_line:
-        for start_p, end_p in line_pair:
-            if start_p in dict_pose_line and end_p in dict_pose_line:
-                start_p = dict_pose_line[start_p]
-                end_p = dict_pose_line[end_p]
-                cv2.line(img, start_p, end_p, line_col, line_size)
-
-    return Image.fromarray(img.astype('uint8'), 'RGB')
-
-
+logger = logging.getLogger()
 
 def main(args):
 
@@ -145,7 +19,7 @@ def main(args):
         os.makedirs(args.output_path)
     global_pid = 0
     vid = 0
-
+    count_list = []
     video_id_dict = defaultdict(list)
     global_id_list= []
 
@@ -157,19 +31,19 @@ def main(args):
 
     video_list= [f for f in annotation]
 
-    print(video_list)
+    logger.info('list of videos: {0}'.format(video_list))
 
     for video_id, video_name in enumerate(video_list):
 
         video_path = list(filter( re.compile('.*'+video_name+'.*'+'.mp4').match, os.listdir(args.input_path)))
         if (len(video_path)==0):
-            print ("Warning: mp4 file not fould")
+            logger.warning('{0} mp4 file not fould'.format(video_name))
             continue
         video_path = os.path.join(args.input_path, video_path[0])
 
         npy_list = list(filter( re.compile('.*'+video_name+'.*').match, os.listdir(args.npy_path)))
         if (len(npy_list)==0):
-            print ("Warning: npy file not fould")
+            logger.warning('{0} npy file not fould'.format(video_name))
             continue
         npy_path = os.path.join(args.npy_path, npy_list[0])
 
@@ -177,31 +51,18 @@ def main(args):
         if annotation is not None:
             valid_frames = annotation[video_name]
 
-        print(video_path)
-        print(npy_path)
-
-        dict_track = np.load(npy_path, allow_pickle=True).item()
-        video_dataset = VideoDataset(video_path)
-
-        assert (video_dataset.num_frames == len(dict_track))
-
-        print("Read %d frames in %s" % (len(dict_track), video_name))
-        print("Now parsing tracklets with sample")
-
-
-        id_dict =  parse_dict_track (video_dataset, dict_track, valid_frames)
-
-        del video_dataset
-
+        folder_name = '%03d_%s' %(video_id, video_name)
 
         if args.split_folder:
-            pose_savepath = os.path.join(args.output_path, 'poses/' + '%3d_%s' %(video_id, video_name))
-            image_savepath = os.path.join(args.output_path, 'images/' + '%3d_%s' %(video_id, video_name))
-            skeleton_savepath = os.path.join(args.output_path, 'skeleton/' + '%3d_%s' %(video_id, video_name))
+            pose_savepath = os.path.join(args.output_path, 'poses/' + folder_name)
+            image_savepath = os.path.join(args.output_path, 'images/' + folder_name)
+            skeleton_savepath = os.path.join(args.output_path, 'skeleton/' + folder_name)
         else:
             pose_savepath = os.path.join(args.output_path, 'poses')
             image_savepath = os.path.join(args.output_path, 'images')
             skeleton_savepath = os.path.join(args.output_path, 'skeleton')
+
+        save_path = [pose_savepath, image_savepath, skeleton_savepath]
 
         if not os.path.exists(pose_savepath):
             os.makedirs(pose_savepath)
@@ -210,55 +71,35 @@ def main(args):
         if args.gen_skeleton and not os.path.exists(skeleton_savepath):
             os.makedirs(skeleton_savepath)
 
-        print('Start writing cropped images and coresponding poses......')
 
-        #for re_idx, (pid, dict_list) in enumerate(id_dict.items()):
+        logger.info("Read video in {0}".format(video_path))
+        logger.info("Read dict track in {0}".format(npy_path))
+
+
+        dict_track = np.load(npy_path, allow_pickle=True).item()
+
+        id_dict = gen_video_detections(args, video_path, save_path, folder_name, dict_track, valid_frames, global_pid)
+
+        video_detections_cnt = 0
         for pid in sorted(id_dict):
-            print('pid %d' % pid)
-            dict_list = id_dict[pid]
-            
-            pid_list=[]
+            global_id_list.append(id_dict[pid])
+            video_detections_cnt += len(id_dict[pid])
 
-            for frame, im, pose in zip( dict_list['frame_idx'], dict_list['images'], dict_list['poses']):
-
-                filename = '%08d' % (global_pid + pid)
-                filename += '_%06d' % frame
-
-                if args.split_folder:
-                    pid_list.append( video_name + '/' + filename + '.jpg')
-                else: 
-                    pid_list.append( filename + '.jpg')
-
-                if not os.path.isfile(os.path.join(pose_savepath, filename+'.txt')) or not args.no_duplicate:
-                    np.savetxt(os.path.join(pose_savepath, filename+'.txt'), pose, fmt="%s")
-
-                if not os.path.isfile(os.path.join(image_savepath, filename+'.jpg')) or not args.no_duplicate:
-                    im.save(os.path.join(image_savepath, filename+'.jpg'),'JPEG')
-
-                if args.gen_skeleton:
-                    if not os.path.isfile(os.path.join(skeleton_savepath, filename+'.jpg')) or not args.no_duplicate:
-                        render_img = draw_pose_cv2(im, pose, pid)
-                        render_img.save(os.path.join(skeleton_savepath, filename+'.jpg'),'JPEG')
-
-            global_id_list.append(pid_list)
-
+        logger.info("Total {0} detections are generated".format(video_detections_cnt))
 
         video_id_dict[video_name].extend(list(range(global_pid, global_pid+len(id_dict))))
         global_pid += len(id_dict)
+        count_list.append(video_detections_cnt)
         vid += 1
-        del id_dict
     
     meta_dict = {}
     meta_dict ['identities'] = global_id_list
     meta_dict ['shot'] = 'single'
     meta_dict ['name'] = 'IdolTrack-Reid'
+    meta_dict ['n_detects'] = count_list
+    meta_dict ['total_detects'] = sum(count_list)
 
-    '''
-    split_dict = {}
-    split_vid = len(video_id_dict)//2
-    split_dict ['query'] = list(range(0, )
-    split_dict ['trainval'] = list(range(global_pid//2, global_pid))
-    '''
+
 
     with open(os.path.join(args.output_path, 'meta.json'), 'w') as fp:
         json.dump(meta_dict, fp)
@@ -272,7 +113,7 @@ if __name__ == '__main__':
 
     parser.add_argument('-i', '--input-path', type=str)
     parser.add_argument('-n', '--npy-path', type=str)
-    parser.add_argument('-a', '--annotation', type=str, default=None)
+    parser.add_argument('-a', '--annotation', type=str)
     parser.add_argument('-o', '--output-path', type=str, default='DanceReiD/')
     parser.add_argument('-nd', '--no-duplicate', type=bool, nargs='?', const=True, default=False)
     parser.add_argument('-gs', '--gen-skeleton', type=bool, nargs='?', const=True, default=False)
---
+++ util.py
@@ -0,0 +1,195 @@
+import os
+from tqdm import tqdm
+from collections import defaultdict 
+import cv2
+
+import logging
+from logging.config import dictConfig
+
+import numpy as np
+from time import time
+from PIL import Image, ImageColor
+
+dict_config_log = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'standard': {
+            'format': '%(asctime)s - [%(levelname)s][%(module)s.%(funcName)s:%(lineno)d] %(message)s',
+            'datefmt': '%Y-%m-%d %H:%M:%S',
+        }
+    },
+
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'standard',
+        },
+        'file': {
+            'filename': 'log.log',
+            'level': 'INFO',
+            'class': 'logging.FileHandler',
+            'formatter': 'standard',
+        }
+    },
+    'loggers': {
+        '__main__': { # logging from this module will be logged in VERBOSE level
+            'handlers': ['console'],
+            'level': 'INFO',
+            'propagate': False,
+        },
+    },
+    'root': {
+        'level': 'DEBUG',
+        'handlers': ['console', 'file'],
+        'propagate': False
+    }
+}
+
+logger = logging.getLogger(__name__)
+dictConfig(dict_config_log)
+
+DICT_POSE_COCO = dict()
+DICT_POSE_COCO['line_pair'] = [
+    (0, 1), (0, 2), (1, 3), (2, 4),  # Head
+    (5, 6), (5, 7), (7, 9), (6, 8), (8, 10),
+    (5, 11), (6, 12),  # Body
+    (11, 13), (12, 14), (13, 15), (14, 16)
+]
+DICT_POSE_COCO['point_col'] = [
+    'green', 'blue', 'blue', 'blue', 'blue', 'yellow', 'orange', 'yellow', 'orange', 'yellow', 'orange',
+    'purple', 'red', 'purple', 'red', 'purple', 'red'
+]
+DICT_POSE_COCO['acc_idx'] = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17)
+DICT_POSE_COCO['flip_ref'] = ((2, 3), (4, 5), (6, 7), (8, 9), (10, 11), (12, 13), (14, 15), (16, 17))
+
+def gen_video_detections (args, video_path, save_path, video_name, dict_track, valids, global_pid, output_height = 256, output_width = 128):
+
+    input_cap = cv2.VideoCapture(video_path)
+    num_frames = int(input_cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    id_dict = defaultdict(list)
+
+    assert (num_frames==len(dict_track))
+
+    start_time = time()
+    logger.info('Start to read frames from {0}'.format(video_path))
+    
+    for frame_idx, dict_poses in tqdm(dict_track.items()):
+        try:
+            ret, image = input_cap.read()
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        except Exception as e:
+            logger.error(e)
+            logger.error('Error occurs at {0}'.format(frame_idx))
+            logger.error('Image: {0}'.format(image))
+
+        for bid, dict_pose in dict_poses.items():
+
+            pid = dict_pose['id'] - 1
+
+            if valids is not None:
+                if frame_idx not in valids[str(pid)]: continue
+
+            box_pos = dict_pose['box_pos']
+            up_left = [box_pos[2], box_pos[0]]
+            bot_right = [box_pos[3], box_pos[1]]
+
+            image_crop, shift_pose = cropBox(image, dict_pose['pose_pos'], up_left, bot_right, output_height, output_width)
+            image_pil = Image.fromarray(image_crop)
+
+            filename = '%04d' % (global_pid + pid)
+            filename += '_%06d' % frame_idx
+
+            if args.split_folder:
+                id_dict[pid].extend( [video_name + '/' + filename + '.jpg'])
+            else: 
+                id_dict[pid].extend( [filename + '.jpg'])
+
+            if not os.path.isfile(os.path.join(save_path[0], filename+'.txt')) or not args.no_duplicate:
+                np.savetxt(os.path.join(save_path[0], filename+'.txt'), shift_pose, fmt="%s")
+
+            if not os.path.isfile(os.path.join(save_path[1], filename+'.jpg')) or not args.no_duplicate:
+                image_pil.save(os.path.join(save_path[1], filename+'.jpg'),'JPEG')
+
+            if args.gen_skeleton:
+                if not os.path.isfile(os.path.join(save_path[2], filename+'.jpg')) or not args.no_duplicate:
+                    render_img = draw_pose_cv2(image_pil, shift_pose, pid)
+                    render_img.save(os.path.join(save_path[2], filename+'.jpg'),'JPEG')
+
+    logger.info('Complete to read {0} frames by {1}'.format(num_frames, time()-start_time))
+
+    return id_dict
+
+def cropBox(img, pose, ul, br, resH, resW):
+
+    im_H = img.shape[0]
+    im_W = img.shape[1]
+
+    lenH = max(br[0] - ul[0], (br[1] - ul[1]) * resH / resW)
+    lenW = lenH * resW / resH
+
+    center = [ (br[0] + ul[0]) / 2, (br[1] + ul[1]) / 2]
+
+    new_ul = [max (0, int(center[0] - lenH / 2)), max (0, int(center[1] - lenW / 2))]
+    new_br = [min (im_H-1, int(center[0] + lenH / 2)), min (im_W-1, int(center[1] + lenW / 2))]
+
+    if new_ul[0] == 0:
+        new_br[0] = lenH
+    elif new_br[0] == im_H-1:
+        new_ul[0] = im_H-lenH
+
+    if new_ul[1] == 0:
+        new_br[1] = lenW
+    elif new_br[1] == im_W-1:
+        new_ul[1] = im_W-lenW
+
+
+    center = [(new_br[0] + new_ul[0]) / 2, (new_br[1] + new_ul[1]) / 2]
+
+    newImg = img[int(new_ul[0]) : int(new_br[0]), int(new_ul[1]) : int(new_br[1])].copy()
+
+    factor = float(resH) / lenH
+    newImg = cv2.resize(newImg, ( int(resW), int(resH) ) )
+
+    shift_pose = pose - np.array([center[1], center[0]])
+    shift_pose = shift_pose * factor + np.array([resW/2, resH/2])
+
+    return newImg, shift_pose
+
+def draw_pose_cv2(img, pose_pos, pid, point_size=5, line_size=2, draw_pose_line=True):
+
+    img = np.asarray(img)
+
+    def convert_int_to_color(i):
+        list_colors = list(ImageColor.colormap)
+        return list_colors[i * 10 % len(list_colors)]
+
+
+    def convert_color_to_rgb(col, default_col='red'):
+        if col in ImageColor.colormap:
+            return ImageColor.getrgb(col)
+        else:
+            return ImageColor.getrgb(default_col)
+
+    line_col = convert_int_to_color(pid)
+
+    dict_pose_line = dict()
+    line_pair = DICT_POSE_COCO['line_pair']
+    point_col = [ImageColor.getrgb(col.strip()) for col in DICT_POSE_COCO['point_col']]
+    line_col = convert_color_to_rgb(line_col)
+
+    for i in range(pose_pos.shape[0]):
+        x, y = int(pose_pos[i, 0]), int(pose_pos[i, 1])
+        if x > 0 and y > 0:
+            dict_pose_line[i] = (x, y)
+            cv2.circle(img, (x, y), point_size, point_col[i], -1)
+
+    if draw_pose_line:
+        for start_p, end_p in line_pair:
+            if start_p in dict_pose_line and end_p in dict_pose_line:
+                start_p = dict_pose_line[start_p]
+                end_p = dict_pose_line[end_p]
+                cv2.line(img, start_p, end_p, line_col, line_size)
+
+    return Image.fromarray(img.astype('uint8'), 'RGB')
